b2145805c90bcf35e0a5dd301cf33b23dec7a446
Varun Ramesh <varunramesh@live.com>
2015-09-03 15:38:53 -0700

    Module autocomplete works

--- data/generate-json.lua
+++ data/generate-json.lua
@@ -4,7 +4,23 @@ package.path = "love-api/?.lua;" .. "dkjson/?.lua;" .. package.path
 local loveapi = require('love_api')
 local json = require('dkjson')
 
+local WIKI_ROOT = 'https://love2d.org/wiki/'
+
 local output = {}
+output['love'] = {
+  type = 'module',
+  description = 'The root LOVE module.',
+  url = WIKI_ROOT .. 'love'
+}
+
+for _, module in ipairs(loveapi.modules) do
+  local name = 'love.' .. module.name
+  output[name] = {
+    type = 'module',
+    description = module.description,
+    url = WIKI_ROOT .. name
+  }
+end
 
 local file = io.open ('api.json', 'w')
 file:write(json.encode(output, { indent = true }))
